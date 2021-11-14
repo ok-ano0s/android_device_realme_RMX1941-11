8b80186e8af5f188c665113eac1cc0ec83341b66
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-14 17:48:46 +0530

    RMX1941: Revamp overlay * Fix status bar height. Add new props and fix battery usage

--- device.mk
+++ device.mk
@@ -30,8 +30,7 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay 
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
@@ -94,10 +93,6 @@ PRODUCT_COPY_FILES += \
 # Symbols
 PRODUCT_PACKAGES += \
     libshim_showlogo
-
-# Overlays -- replace official
-PRODUCT_PACKAGES += \
-    FrameworkResOverlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := xxxhdpi
