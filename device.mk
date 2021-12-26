cb6a001a6dd6951c33476980512cacc88f3df840
cArN4gEisDeD <carn4geisd3d@gmail.com>
2021-12-26 22:15:26 +0530

    RMX1941: Drop libhidl
    Signed-off-by: cArN4gEisDeD <carn4geisd3d@gmail.com>

--- device.mk
+++ device.mk
@@ -50,8 +50,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0_system \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
