98f2638c62cf09f6ea4baadd0d9c8e219222187e
cArN4gEisDeD <carn4geisd3d@gmail.com>
2022-01-15 22:22:34 +0530

    RMX1941: Drop Telephony Boot jars
    Signed-off-by: cArN4gEisDeD <carn4geisd3d@gmail.com>

--- device.mk
+++ device.mk
@@ -113,19 +113,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.RMX1941
 
-# Telephony
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common    
-
-PRODUCT_PACKAGES += \
-    ImsServiceBase
-
 # ImsInit hack
 PRODUCT_PACKAGES += \
     ImsInit
